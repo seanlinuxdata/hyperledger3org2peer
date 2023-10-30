66acb145eff493af9770fe3ec37f90f204cd158d
Hyun Seo Joo(Sean Joo) <112134402+seanjoo0322@users.noreply.github.com>
2023-10-30 13:01:22 +0900

    Update deployCC.sh

--- test-networkFINAL/scripts/deployCC.sh
+++ test-networkFINAL/scripts/deployCC.sh
@@ -91,9 +91,6 @@ approveForMyOrg 1
 
 ## check whether the chaincode definition is ready to be committed
 ## expect org1 to have approved and org2 not to
-checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": false"
-checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": false"
-checkCommitReadiness 3 "\"Org1MSP\": true" "\"Org3MSP\": false"
 
 ## now approve also for org2
 approveForMyOrg 2
@@ -101,22 +98,23 @@ approveForMyOrg 3
 
 ## check whether the chaincode definition is ready to be committed
 ## expect them both to have approved
-checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": true"
-checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": true"
+#checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": true"
+#checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": true"
 
 ## now that we know for sure both orgs have approved, commit the definition
-commitChaincodeDefinition 1 2
+commitChaincodeDefinition 1 2 3
 
 ## query on both orgs to see that the definition committed successfully
 queryCommitted 1
 queryCommitted 2
+queryCommitted 3
 
 ## Invoke the chaincode - this does require that the chaincode have the 'initLedger'
 ## method defined
 if [ "$CC_INIT_FCN" = "NA" ]; then
   infoln "Chaincode initialization is not required"
 else
-  chaincodeInvokeInit 1 2
+  chaincodeInvokeInit 1 2 3
 fi
 
 exit 0
